95bb886b769155da3136a0fad2fdbd1be29a9858
Awais-PC\Administrator <awaisilyas@live.com>
2025-02-18 15:06:56 +0000

    validation and test cases added

--- PVUnitTests/ZoneDeterminerUnitTests.cs
+++ PVUnitTests/ZoneDeterminerUnitTests.cs
@@ -4,11 +4,31 @@ namespace PVUnitTests
 {
     public class Tests
     {
+        private DetermineZone determineZone;
         [SetUp]
         public void Setup()
         {
+            determineZone = new DetermineZone();
         }
 
+        [Test]
+        public void WhenNullObjectPassed_ThenItReturnedArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => determineZone.Determine(null));
+        }
+        [Test]
+        [TestCase(51, 0)]
+        [TestCase(-0.1, 0)]
+        [TestCase(0, 101)]
+        [TestCase(0, -0.1)]
+        public void WhenOutOfRangeParametersPasssed_ThenItReturnedArgumentOutOfRangeException(double averageAlarmrate,double percentageOutsideTarget)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => 
+            determineZone.Determine(new ZoneDeterminerParameters() 
+            {
+                AverageAlarmRate = averageAlarmrate, PercentageOutsideTarget = percentageOutsideTarget
+            }));
+        }
         [Test]
         public void WhenZoneDeterminerParametersPassedWithValues_ThenCorrectResultreturned()
         {
@@ -17,8 +37,7 @@ namespace PVUnitTests
                 AverageAlarmRate = 0.5,
                 PercentageOutsideTarget = 1
             };
-
-            var determineZone = new DetermineZone();
+                        
             var result=determineZone.Determine(zoneDeterminerParameters);
 
             Assert.That(result, Is.EqualTo("Robust"));
--- ZoneDeterminer/DetermineZone.cs
+++ ZoneDeterminer/DetermineZone.cs
@@ -10,12 +10,37 @@
     {
         public string Determine(ZoneDeterminerParameters zoneDeterminerParameters)
         {
+            ValidateInput(zoneDeterminerParameters);
+
             if (zoneDeterminerParameters.AverageAlarmRate < 1)
             {
                 if (zoneDeterminerParameters.PercentageOutsideTarget <= 1)
                     return "Robust";
             }
             return "Stable";
+        }
+
+        private void ValidateInput(ZoneDeterminerParameters zoneDeterminerParameters)
+        {
+
+            ArgumentNullException.ThrowIfNull(zoneDeterminerParameters, nameof(zoneDeterminerParameters));
+
+            if (!zoneDeterminerParameters.AverageAlarmRate.IsInRange(0, 50))
+            {
+                throw new ArgumentOutOfRangeException(nameof(zoneDeterminerParameters.AverageAlarmRate));
+            }
+            if (!zoneDeterminerParameters.PercentageOutsideTarget.IsInRange(0, 100))
+            {
+                throw new ArgumentOutOfRangeException(nameof(zoneDeterminerParameters.AverageAlarmRate));
+            }
+
+        }
+    }
+    public static class InRangeExtension
+    {
+        public static bool IsInRange(this double value, double min, double max)
+        {
+            return value >= min && value <= max;
         }
     }
 }
