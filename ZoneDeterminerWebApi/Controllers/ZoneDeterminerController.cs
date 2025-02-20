f2f004cc56c53cf5a6c7478d37439994193e689e
Awais-PC\Administrator <awaisilyas@live.com>
2025-02-20 16:52:40 +0000

    webapi added to test the zone determiner

---
+++ ZoneDeterminerWebApi/Controllers/ZoneDeterminerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZoneDeterminer;
+
+namespace ZoneDeterminerWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ZoneDeterminerController : Controller
+    {
+        private readonly IDetermineZone _determineZone;
+        private readonly ILogger<ZoneDeterminerController> logger;
+
+        public ZoneDeterminerController(IDetermineZone determineZone, ILogger<ZoneDeterminerController> logger)
+        {
+            _determineZone = determineZone;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetZoneDetermine([FromBody] ZoneDeterminerParameters zoneDeterminerParameters)
+        {
+            return Ok(_determineZone.Determine(zoneDeterminerParameters));
+        }
+    }
+}
